f59b75bb806f09a3f07577fb557688dfd7db2521
Varma2912 <chaitanyavarma2912@gmail.com>
2026-06-08 18:00:59 +0530

    Removed stopwords.pkl dependency

--- StreamlitQuora-app/helper.py
+++ StreamlitQuora-app/helper.py
@@ -4,6 +4,8 @@ import distance
 from fuzzywuzzy import fuzz
 import pickle
 import numpy as np
+import nltk
+nltk.download('stopwords')
 
 cv = pickle.load(open('cv.pkl','rb'))
 
@@ -22,7 +24,9 @@ def test_total_words(q1,q2):
 def test_fetch_token_features(q1, q2):
     SAFE_DIV = 0.0001
 
-    STOP_WORDS = pickle.load(open('stopwords.pkl','rb'))
+    from nltk.corpus import stopwords
+
+    STOP_WORDS = set(stopwords.words('english'))
 
     token_features = [0.0] * 8
 
